9a48d3532e8d27edfb1ed4045e4c0efbda495b6b
özkan yılmaz <ozknylmz90@gmail.com>
2024-09-03 12:26:08 +0300

    edit folder in project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,8 +13,8 @@ const About = () => {
         <motion.div
         whileInView={{opacity:1,x:0}}
         initial={{opacity:0 , x:-100}}
+        transition={{duration:1}}
         className="w-full lg:w-1/2 lg:p-8">
-    
           <div className="flex items-center justify-center"></div>
           <img className="rounded-2xl" src={aboutImg} alt="about" />
         </motion.div>
